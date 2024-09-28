54d29201ef51788af8525cf693cd2474e58fdbbe
Kovalev Vitaliy <kvp94best@gmail.com>
2024-09-28 12:12:15 +0700

    Move users to separate module.

---
+++ modules/my-modules/users.scm
@@ -0,0 +1,16 @@
+(define-module (my-modules users)
+  #:use-module (gnu system accounts)
+  #:use-module (gnu system shadow)
+  #:export (users))
+
+
+(define users
+  (cons*
+   (user-account
+    (name "kovalev")
+    (comment "Виталий Ковалёв")
+    (group "users")
+    (home-directory "/home/kovalev")
+    (supplementary-groups
+     '("wheel" "netdev" "audio" "video" "kvm" "libvirt" "docker")))
+   %base-user-accounts))
--- systems/mirage-herbst.scm
+++ systems/mirage-herbst.scm
@@ -1,14 +1,3 @@
-;; This is an operating system configuration generated
-;; by the graphical installer.
-;;
-;; Once installation is complete, you can learn and modify
-;; this file to tweak the system configuration, and pass it
-;; to the 'guix system reconfigure' command to effect your
-;; changes.
-
-
-;; Indicate which modules to import to access the variables
-;; used in this configuration.
 (use-modules (gnu)
              (nongnu packages linux)
              (nongnu system linux-initrd)
@@ -16,6 +5,7 @@
              ((my-modules services) #:prefix my:)
              ((my-modules keyboard) #:prefix my:)
              ((my-modules filesystem) #:prefix my:)
+             ((my-modules users) #:prefix my:)
              (my-modules hosts)
              (my-modules hosts other)
              ((my-modules hosts mts xring) #:prefix xring:))
@@ -52,26 +42,18 @@
   (keyboard-layout my:kb-layout)
   (host-name "mirage")
 
-  (users
-   (cons*
-    (user-account
-     (name "kovalev")
-     (comment "Виталий Ковалёв")
-     (group "users")
-     (home-directory "/home/kovalev")
-     (supplementary-groups
-      '("wheel" "netdev" "audio" "video" "kvm" "libvirt" "docker")))
-    %base-user-accounts))
-
+  (users my:users)
   ;Globaly installed packages(e.g. for all users)
   (packages my:system-packages)
   ;Installed and enabled services(like ssh-server,docker, etc.)
   (services my:system-services)
 
   (setuid-programs
-   (append (list (setuid-program
-                  (program (file-append spice-gtk "/libexec/spice-client-glib-usb-acl-helper"))))
-           %setuid-programs))
+   (append
+    (list
+     (setuid-program
+      (program (file-append spice-gtk "/libexec/spice-client-glib-usb-acl-helper"))))
+    %setuid-programs))
 
   ;Required for LVM disks
   (mapped-devices my:lvm-mapped-devices)
