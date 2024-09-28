a66f0c0aeb88bf82bd107f41d07614911997ad6e
Kovalev Vitaliy <kvp94best@gmail.com>
2024-09-28 12:02:38 +0700

    Move file systems to separate module.

---
+++ modules/my-modules/filesystem.scm
@@ -0,0 +1,40 @@
+(define-module (my-modules filesystem)
+  #:use-module (gnu system mapped-devices)
+  #:use-module (gnu system file-systems)
+  #:export (lvm-mapped-devices
+            file-systems
+            swap-devices))
+
+
+(define lvm-mapped-devices
+  (list
+   (mapped-device
+    (source "MirageLinux")
+    (targets
+     (list
+      "MirageLinux-GuixRootSecond"
+      "MirageLinux-GuixHomeSecond"
+      "MirageLinux-Swap"))
+    (type lvm-device-mapping))))
+
+(define file-systems
+  (cons*
+   (file-system
+    (mount-point "/home")
+    (device "/dev/mapper/MirageLinux-GuixHomeSecond")
+    (type "ext4"))
+   (file-system
+    (mount-point "/")
+    (device "/dev/mapper/MirageLinux-GuixRootSecond")
+    (type "ext4"))
+   (file-system
+    (mount-point "/boot/efi")
+    (device (uuid "BFD6-6AB9" 'fat32))
+    (type "vfat"))
+   %base-file-systems))
+
+(define swap-devices
+  (list
+   (swap-space
+    (target (file-system-label "swap"))
+    (dependencies lvm-mapped-devices))))
--- systems/mirage-herbst.scm
+++ systems/mirage-herbst.scm
@@ -15,10 +15,10 @@
              ((my-modules packages) #:prefix my:)
              ((my-modules services) #:prefix my:)
              ((my-modules keyboard) #:prefix my:)
+             ((my-modules filesystem) #:prefix my:)
              (my-modules hosts)
              (my-modules hosts other)
              ((my-modules hosts mts xring) #:prefix xring:))
-
 
 (use-package-modules
  spice
@@ -35,14 +35,8 @@
    (append
     (list "i915.enable_psr=0")
     %default-kernel-arguments))
-
   (initrd microcode-initrd)
   (firmware (list linux-firmware sof-firmware))
-  (locale "ru_RU.utf8")
-  (timezone "Asia/Novosibirsk")
-  ;keyboard-layout will use in several places in this config
-  (keyboard-layout my:kb-layout)
-
   (bootloader
    (bootloader-configuration
     (bootloader grub-efi-bootloader)
@@ -52,6 +46,10 @@
             (gfxmode '("1920x1080x32" "auto"))))
     (keyboard-layout my:kb-layout)))
 
+  (locale "ru_RU.utf8")
+  (timezone "Asia/Novosibirsk")
+  ;keyboard-layout will use in several places in this config
+  (keyboard-layout my:kb-layout)
   (host-name "mirage")
 
   (users
@@ -65,54 +63,18 @@
       '("wheel" "netdev" "audio" "video" "kvm" "libvirt" "docker")))
     %base-user-accounts))
 
-
   ;Globaly installed packages(e.g. for all users)
   (packages my:system-packages)
   ;Installed and enabled services(like ssh-server,docker, etc.)
   (services my:system-services)
-
 
   (setuid-programs
    (append (list (setuid-program
                   (program (file-append spice-gtk "/libexec/spice-client-glib-usb-acl-helper"))))
            %setuid-programs))
 
-
-  ;Required for LVM disks 
-  (mapped-devices 
-   (list
-    (mapped-device
-     (source "MirageLinux")
-     (targets
-      (list
-       "MirageLinux-GuixRootSecond"
-       "MirageLinux-GuixHomeSecond"
-       "MirageLinux-Swap"))
-     (type lvm-device-mapping))))
-
-
-  (file-systems
-   (cons*
-;This is OS file systems
-    (file-system
-      (mount-point "/home")
-      (device "/dev/mapper/MirageLinux-GuixHomeSecond")
-      (type "ext4"))
-
-    (file-system
-      (mount-point "/")
-      (device "/dev/mapper/MirageLinux-GuixRootSecond")
-      (type "ext4"))
-
-    (file-system
-      (mount-point "/boot/efi")
-      (device (uuid "BFD6-6AB9" 'fat32))
-      (type "vfat"))
-;Other OS file systems
-%base-file-systems))
-
-
-  (swap-devices
-   (list
-    (swap-space (target (file-system-label "swap"))
-                (dependencies mapped-devices)))))
+  ;Required for LVM disks
+  (mapped-devices my:lvm-mapped-devices)
+  ;OS file systems
+  (file-systems my:file-systems)
+  (swap-devices my:swap-devices))
