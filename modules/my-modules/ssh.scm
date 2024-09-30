8863ae954fd28ad04ce25d5a93b6f1b6893519c8
Kovalev Vitaliy <kvp94best@gmail.com>
2024-09-30 12:18:30 +0700

    Add ssh module to my-modules with ssh_config host definitions. Update mts submodule repo(ssh module was added too).

---
+++ modules/my-modules/ssh.scm
@@ -0,0 +1,37 @@
+(define-module (my-modules ssh)
+  #:use-module (gnu home services ssh)
+  #:export (ssh-hosts))
+
+
+(define my-servers
+  (list
+   (openssh-host
+    (name "vpnserv")
+    (host-name "vpn.web-wyrd.space")
+    (user "vpn_admin")
+    (port 56713)
+    (identity-file "~/.ssh/keys/vpnserv"))))
+
+(define ipoint
+  (list
+   (openssh-host
+    (name "ipoint-*")
+    (port 57655)
+    (user "lanadmin")
+    (identity-file "~/.ssh/keys/ipoint"))))
+
+(define akadem
+  (list
+   (openssh-host
+    (name "breadrobot")
+    (user "breadrobot"))
+   (openssh-host
+    (name "orangepi3")
+    (user "admin"))))
+
+
+(define ssh-hosts
+  (append
+   my-servers
+   ipoint
+   akadem))
