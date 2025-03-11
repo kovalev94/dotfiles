79c24799268ebe2bd116da8dcba6af1df4ac6c47
Kovalev Vitaliy <kvp94best@gmail.com>
2025-03-11 10:04:20 +0700

    Add system configuration for guix-installer.

---
+++ systems/guix-installer/system-pure.scm
@@ -0,0 +1,93 @@
+(use-modules (gnu)
+             (gnu services networking)
+             (gnu services desktop)
+             (gnu services ssh)
+             (gnu services avahi)
+             ((kovalev package-lists mirage) #:prefix mirage-packages:)
+             ((kovalev package-lists kovalev) #:prefix kovalev-packages:)
+             (kovalev services)
+             (kovalev keyboard)
+             ((srfi srfi-1) #:prefix srfi-1:))
+
+
+(operating-system
+  (host-name "guix-builder")
+  (locale "ru_RU.utf8")
+  (timezone "Asia/Novosibirsk")
+
+  (bootloader
+   (bootloader-configuration
+    (bootloader grub-bootloader)
+    (targets '("/dev/sda"))
+    (keyboard-layout kb-layout)))
+
+  (kernel-arguments (list "console=ttyS0,115200"))
+  (initrd-modules (append (list "virtio_scsi")
+                          %base-initrd-modules))
+
+
+  (keyboard-layout kb-layout)
+
+
+  (users
+   (cons
+    (user-account
+     (name "manager")
+     (comment "Build Manager")
+     (group "users")
+     (supplementary-groups
+      '("wheel" "netdev" "audio" "video")))
+    %base-user-accounts))
+
+
+  (packages
+   (append
+    (map (compose list specification->package+output)
+         (srfi-1:delete
+          "firefox"
+          (append
+           mirage-packages:all
+           kovalev-packages:all)))
+    %base-packages))
+
+
+  (services
+   (append
+    (list
+     (service iptables-service-type
+              (iptables-configuration
+               (ipv4-rules (local-file "iptables.rules"))
+               (ipv6-rules (local-file "iptables.rules"))))
+     (service openssh-service-type
+              (openssh-configuration
+               (port-number 13131))))
+
+    (modify-services (remove-services
+                      (list
+                       avahi-service-type
+                       nm-applet-service-type
+                       usb-modeswitch-service-type)
+                      desktop-without-gdm-service-list)
+
+      (guix-service-type config =>
+                         (guix-configuration
+                          (inherit config)
+                          (substitute-urls
+                           (srfi-1:delete "https://ci.guix.gnu.org"
+                                          %default-substitute-urls))))
+      (console-font-service-type _ =>
+                                 hi-dpi-console-font-configuration))))
+
+
+  (file-systems
+   (cons
+    (file-system
+     (device (file-system-label "root"))
+     (mount-point "/")
+     (type "ext4"))
+    %base-file-systems))
+
+  (swap-devices
+   (list
+    (swap-space
+     (target (file-system-label "swap"))))))
