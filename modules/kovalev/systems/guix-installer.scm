a4cab9ae96dfb610e3e1e97f789f8e0fb4ebc60c
Kovalev Vitaliy <kvp94best@gmail.com>
2025-03-30 17:25:44 +0700

    Move system definitions to modules. Also delete boris system definition.

---
+++ modules/kovalev/systems/guix-installer.scm
@@ -0,0 +1,72 @@
+(define-module (kovalev system guix-installer)
+  #:use-modules (gnu)
+  #:use-modules (gnu services networking)
+  #:use-modules (gnu services desktop)
+  #:use-modules (gnu services ssh)
+  #:use-modules (gnu packages package-management)
+  #:use-modules (nongnu system install)
+  #:use-modules (nongnu system linux-initrd)
+  #:use-modules (nongnu packages linux)
+  #:use-modules ((kovalev package-lists mirage) #:prefix mirage-packages:)
+  #:use-modules ((kovalev package-lists kovalev) #:prefix kovalev-packages:)
+  #:use-modules (kovalev services)
+  #:use-modules (kovalev keyboard)
+  #:use-modules ((srfi srfi-1) #:prefix srfi-1:))
+
+
+(define-public guix-installer-system
+  (operating-system
+   (inherit installation-os-nonfree)
+
+   (host-name "guix-installer-kovalev")
+   (locale "ru_RU.utf8")
+   (timezone "Asia/Novosibirsk")
+
+   (bootloader
+    (bootloader-configuration
+     (bootloader grub-bootloader)
+     (targets '("/dev/sda"))
+     (keyboard-layout kb-layout)))
+
+                                        ; For unknown for me reasons, it breaks booting
+                                        ;(initrd microcode-initrd)
+
+   (firmware
+    (list
+     linux-firmware
+     sof-firmware))
+
+   (keyboard-layout kb-layout)
+
+
+   (packages
+    (append
+     (map (compose list specification->package+output)
+          (append
+           mirage-packages:all
+           kovalev-packages:all))
+     %base-packages))
+
+   (services
+    (append
+     (list
+      (service iptables-service-type
+               (iptables-configuration
+                (ipv4-rules (local-file "../../../../sys-files/iptables/guix-installer.rules"))
+                (ipv6-rules (local-file "../../../../sys-files/iptables/guix-installer.rules")))))
+
+     (modify-services (operating-system-user-services installation-os-nonfree)
+                      (openssh-service-type config =>
+                                            (openssh-configuration
+                                             (inherit config)
+                                             (port-number 13131)
+                                             (%auto-start? #t)))
+                      (guix-service-type _ =>
+                                         (guix-configuration
+                                          (channels default-channels-with-nonguix)
+                                          (guix (guix-for-channels default-channels-with-nonguix))
+                                          (substitute-urls bordeaux-nonguix-substitute-urls)
+                                          (authorized-keys default-authorized-keys-with-nonguix))))))))
+
+
+guix-installer-system
---
+++ modules/kovalev/systems/mirage.scm
@@ -0,0 +1,151 @@
+(define-module (kovalev systems mirage)
+  #:use-module (gnu)
+  #:use-module (gnu services networking)
+  #:use-module (gnu services desktop)
+  #:use-module (gnu services ssh)
+  #:use-module (gnu services avahi)
+  #:use-module (gnu system setuid)
+  #:use-module (gnu packages spice)
+  #:use-module (gnu packages networking)
+  #:use-module (gnu packages package-management)
+  #:use-module (nongnu packages linux)
+  #:use-module (nongnu system linux-initrd)
+  #:use-module (kovalev package-lists mirage)
+  #:use-module (kovalev etc-hosts)
+  #:use-module (kovalev services)
+  #:use-module (kovalev keyboard)
+  #:use-module ((srfi srfi-1) #:prefix srfi-1:))
+
+(define-public mirage-system
+  (operating-system
+   (host-name "mirage")
+   (locale "ru_RU.utf8")
+   (timezone "Asia/Novosibirsk")
+
+
+   (bootloader
+    (bootloader-configuration
+     (bootloader grub-efi-bootloader)
+     (targets '("/boot/efi"))
+     (theme (grub-theme
+             (inherit (grub-theme))
+             (gfxmode '("1920x1080x32" "auto"))))
+     (keyboard-layout kb-layout)))
+
+   (kernel linux)
+   ;Temporary(I hope) fix for screen redraw lags
+   (kernel-arguments
+    (append
+     (list "i915.enable_psr=0")
+     %default-kernel-arguments))
+
+   (initrd microcode-initrd)
+
+   (firmware
+    (list
+     linux-firmware
+     sof-firmware))
+
+
+   (keyboard-layout kb-layout)
+
+
+   (users
+    (cons*
+     (user-account
+      (name "kovalev")
+      (comment "Виталий Ковалёв")
+      (group "users")
+      (home-directory "/home/kovalev")
+      (supplementary-groups
+       '("wheel" "netdev" "audio" "video" "kvm" "libvirt" )))
+     %base-user-accounts))
+
+                                        ;Globaly installed packages(e.g. for all users)
+   (packages
+    (append
+     (map (compose list specification->package+output)
+          (append
+           system-utils
+           network-utils
+           base-toolkit
+           base-gui))
+     %base-packages))
+
+                                        ;Installed and enabled services(like ssh-server,docker, etc.)
+   (services
+    (append
+     (assoc-ref virtualization-service-list "services")
+     (list
+      (service bluetooth-service-type)
+      (service iptables-service-type
+               (iptables-configuration
+                (ipv4-rules (local-file "../../../../sys-files/iptables/mirage.rules"))
+                (ipv6-rules (local-file "../../../../sys-files/iptables/mirage.rules"))))
+      (simple-service 'add-extra-hosts
+                      hosts-service-type
+                      (append
+                       ipoint
+                       akadem))
+      (service openssh-service-type
+               (openssh-configuration
+                (port-number 13131))))
+
+     (modify-services shit-trimmed-desktop-services
+                      (guix-service-type _ =>
+                                         (guix-configuration
+                                          (channels default-channels-with-nonguix)
+                                          (guix (guix-for-channels default-channels-with-nonguix))
+                                          (substitute-urls bordeaux-nonguix-subsitute-urls)
+                                          (authorized-keys default-authorized-keys-with-nonguix)))
+                      (console-font-service-type _ =>
+                                                 hi-dpi-console-font-configuration)
+                      (network-manager-service-type _ =>
+                                                    network-manager-with-vpnc-configuration))))
+
+
+   (setuid-programs
+    (append
+     (cons*
+      (setuid-program
+       (program
+        (file-append iputils "/bin/ping")));Neeeded for iptuils ping
+      (assoc-ref virtualization-service-list "setuid-programs"))
+     %setuid-programs))
+
+                                        ;Required for LVM disks
+   (mapped-devices
+    (list
+     (mapped-device
+      (source "MirageLinux")
+      (targets
+       (list
+        "MirageLinux-GuixRoot"
+        "MirageLinux-GuixHome"
+        "MirageLinux-Swap"))
+      (type lvm-device-mapping))))
+
+   (file-systems
+    (cons*
+     (file-system
+      (mount-point "/home")
+      (device "/dev/mapper/MirageLinux-GuixHome")
+      (type "ext4"))
+     (file-system
+      (mount-point "/")
+      (device "/dev/mapper/MirageLinux-GuixRoot")
+      (type "ext4"))
+     (file-system
+      (mount-point "/boot/efi")
+      (device (uuid "9E98-8E5C" 'fat32))
+      (type "vfat"))
+     %base-file-systems))
+
+   (swap-devices
+    (list
+     (swap-space
+      (target (file-system-label "swap"))
+      (dependencies mapped-devices))))))
+
+
+mirage-system
