57dc4970f070fa4920cafb4f77e401a36b5fe985
Kovalev Vitaliy <kvp94best@gmail.com>
2025-11-26 10:56:05 +0700

    Format vitaliy-kovalev home definition.

--- guix-config/homes/vitaliy-kovalev.scm
+++ guix-config/homes/vitaliy-kovalev.scm
@@ -25,104 +25,104 @@
 ;; See the "Replicating Guix" section in the manual.
 (define-public vitaliy.kovalev-home
   (home-environment
-    ;; Below is the list of packages that will show up in your
-    ;; Home profile, under ~/.guix-home/profile.
-    (packages
-     (specifications->packages
-      (append
-       (list
-        "sipp"
-        "linphone-desktop"
-        "wireshark")
-       herbst-de
-       emacs-base
-       python-emacs
-       mail-emacs
-       golang-emacs
-       video-tools)))
+   ;; Below is the list of packages that will show up in your
+   ;; Home profile, under ~/.guix-home/profile.
+   (packages
+    (specifications->packages
+     (append
+      (list
+       "sipp"
+       "linphone-desktop"
+       "wireshark")
+      herbst-de
+      emacs-base
+      python-emacs
+      mail-emacs
+      golang-emacs
+      video-tools)))
 
-    ;; Below is the list of Home services.  To search for available
-    ;; services, run 'guix home search KEYWORD' in a terminal.
-    (services
-     (list
-      (service home-batsignal-service-type)
-      (service home-ssh-agent-service-type)
-      (service home-gpg-agent-service-type
-               (home-gpg-agent-configuration
-                 (pinentry-program
-                  (file-append
-                   (specification->package "pinentry-rofi")
-                   "/bin/pinentry-rofi"))
-                 (default-cache-ttl 6000)
-                 (max-cache-ttl 7200)))
-      (service home-startx-command-service-type
-               (for-home (xorg-configuration
-                           (keyboard-layout kb-layout))))
-      (service home-bash-service-type
-               (home-bash-configuration
-                 (aliases '(("grep" . "grep --color=auto") ("ll" . "ls -l")
-                            ("ls" . "ls -p --color=auto")))
-                 (bashrc (list
-                          (local-file
-                           (string-append
-                            (getenv "DOTFILES_DIR")
-                            "/home-files/general/.bashrc")
-                           "bashrc")))
-                 (bash-profile (list
-                                (local-file
-                                 (string-append
-                                  (getenv "DOTFILES_DIR")
-                                  "/home-files/general/.bash_profile")
-                                 "bash_profile")))))
+   ;; Below is the list of Home services.  To search for available
+   ;; services, run 'guix home search KEYWORD' in a terminal.
+   (services
+    (list
+     (service home-batsignal-service-type)
+     (service home-ssh-agent-service-type)
+     (service home-gpg-agent-service-type
+              (home-gpg-agent-configuration
+               (pinentry-program
+                (file-append
+                 (specification->package "pinentry-rofi")
+                 "/bin/pinentry-rofi"))
+               (default-cache-ttl 6000)
+               (max-cache-ttl 7200)))
+     (service home-startx-command-service-type
+              (for-home (xorg-configuration
+                         (keyboard-layout kb-layout))))
+     (service home-bash-service-type
+              (home-bash-configuration
+               (aliases '(("grep" . "grep --color=auto") ("ll" . "ls -l")
+                          ("ls" . "ls -p --color=auto")))
+               (bashrc (list
+                        (local-file
+                         (string-append
+                          (getenv "DOTFILES_DIR")
+                          "/home-files/general/.bashrc")
+                         "bashrc")))
+               (bash-profile (list
+                              (local-file
+                               (string-append
+                                (getenv "DOTFILES_DIR")
+                                "/home-files/general/.bash_profile")
+                               "bash_profile")))))
 
-      (service home-dotfiles-service-type
-               (home-dotfiles-configuration
-                 (source-directory
-                  (string-append
-                   (getenv "DOTFILES_DIR") "/home-files"))
-                 (directories '("general" "vitaliy.kovalev"))
-                 (excluded
-                  (list
-                   ".*~"
-                   ".*\\.swp"
-                   "\\.git"
-                   "\\.gitignore"
-                   ;;Exclude .bash* files because they are
-                   ;;already managed by home-bash-service
-                   ".bashrc"
-                   ".bash_profile"))))
+     (service home-dotfiles-service-type
+              (home-dotfiles-configuration
+               (source-directory
+                (string-append
+                 (getenv "DOTFILES_DIR") "/home-files"))
+               (directories '("general" "vitaliy.kovalev"))
+               (excluded
+                (list
+                 ".*~"
+                 ".*\\.swp"
+                 "\\.git"
+                 "\\.gitignore"
+                 ;;Exclude .bash* files because they are
+                 ;;already managed by home-bash-service
+                 ".bashrc"
+                 ".bash_profile"))))
 
 
-      (service home-msmtp-service-type
-               (home-msmtp-configuration
-                 (defaults
+     (service home-msmtp-service-type
+              (home-msmtp-configuration
+               (defaults
+                 (msmtp-configuration
+                  (tls? #t)
+                  (auth? #t)))
+               (accounts
+                (list
+                 (msmtp-account
+                  (name "Eltex")
+                  (configuration
                    (msmtp-configuration
-                     (tls? #t)
-                     (auth? #t)))
-                 (accounts
-                  (list
-                   (msmtp-account
-                     (name "Eltex")
-                     (configuration
-                      (msmtp-configuration
-                        (host "smtp.eltex.loc")
-                        (port 587)
-                        (user "vitaliy.kovalev@eltex.loc")
-                        (from "vitaliy.kovalev@eltex.loc")
-                        (password-eval "pass Eltex/LDAP/vitaliy.kovalev"))))))))
+                    (host "smtp.eltex.loc")
+                    (port 587)
+                    (user "vitaliy.kovalev@eltex.loc")
+                    (from "vitaliy.kovalev@eltex.loc")
+                    (password-eval "pass Eltex/LDAP/vitaliy.kovalev"))))))))
 
-      (service home-openssh-service-type
-               (home-openssh-configuration
-                 (hosts
-                  (append
-                   vpn-servers
-                   my-version-control))
-                 (authorized-keys (list
-                                   (local-file
-                                    (string-append
-                                     (getenv "DOTFILES_DIR")
-                                     "/home-files/general/.ssh/keys/gawain.pub")
-                                    "gawain.pub")))
-                 (add-keys-to-agent "120m")))))))
+     (service home-openssh-service-type
+              (home-openssh-configuration
+               (hosts
+                (append
+                 vpn-servers
+                 my-version-control))
+               (authorized-keys (list
+                                 (local-file
+                                  (string-append
+                                   (getenv "DOTFILES_DIR")
+                                   "/home-files/general/.ssh/keys/gawain.pub")
+                                  "gawain.pub")))
+               (add-keys-to-agent "120m")))))))
 
 vitaliy.kovalev-home
