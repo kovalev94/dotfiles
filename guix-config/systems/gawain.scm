fbc7b1dc0f90c3f5d866baf8863b313e6d97363c
Kovalev Vitaliy <kvp94best@gmail.com>
2025-12-01 09:17:56 +0700

    Format gawain.scm

--- guix-config/systems/gawain.scm
+++ guix-config/systems/gawain.scm
@@ -26,167 +26,167 @@
 
 (define-public gawain-system
   (operating-system
-    (host-name "gawain")
-    (locale "ru_RU.utf8")
-    (timezone "Asia/Novosibirsk")
+   (host-name "gawain")
+   (locale "ru_RU.utf8")
+   (timezone "Asia/Novosibirsk")
 
-    (bootloader
-      (bootloader-configuration
-        (bootloader grub-efi-bootloader)
-        (targets '("/boot/efi"))
-        (theme (grub-theme
-                (inherit (grub-theme))
-                (gfxmode '("1920x1080x32" "auto"))))
-        (keyboard-layout kb-layout)))
+   (bootloader
+    (bootloader-configuration
+     (bootloader grub-efi-bootloader)
+     (targets '("/boot/efi"))
+     (theme (grub-theme
+             (inherit (grub-theme))
+             (gfxmode '("1920x1080x32" "auto"))))
+     (keyboard-layout kb-layout)))
 
-    (kernel linux)
-    (initrd microcode-initrd)
-    (firmware
-     (list
-      linux-firmware
-      sof-firmware))
+   (kernel linux)
+   (initrd microcode-initrd)
+   (firmware
+    (list
+     linux-firmware
+     sof-firmware))
 
-    (keyboard-layout kb-layout)
+   (keyboard-layout kb-layout)
 
-    (users
-     (cons*
-      (user-account
-        (name "vitaliy.kovalev")
-        (comment "Виталий Ковалёв")
-        (uid 1000)
-        (group "users")
-        (home-directory "/home/vitaliy.kovalev")
-        (supplementary-groups
-         '("wheel" "netdev" "audio" "video" "kvm" "libvirt" )))
-      %base-user-accounts))
-    ;;Globaly installed packages(e.g. for all users)
-    (packages
-     (append
-      (specifications->packages
-       (cons*
-        "eltex-certs"
-        (append
-         base-sys
-         base-gui
-         fs-tools
-         network-tools
-         virtualization-base
-         sys-fonts)))
-      %base-packages))
-
-    ;;Installed and enabled services(like ssh-server,docker, etc.)
-    (services
-     (append
-      (assoc-ref virtualization-service-list "services")
-      (list
-       (service bluetooth-service-type)
-       (service iptables-service-type
-                (iptables-configuration
-                  (ipv4-rules
-                   (local-file
-                    (string-append
-                     (getenv "DOTFILES_DIR")
-                     "/sys-files/gawain/iptables.rules")))
-                  (ipv6-rules
-                   (local-file
-                    (string-append
-                     (getenv "DOTFILES_DIR")
-                     "/sys-files/gawain/iptables.rules")))))
-       (simple-service 'dotfiles-and-guix-env session-environment-service-type
-                       `(("DOTFILES_DIR" .
-                          "/home/vitaliy.kovalev/.dotfiles")
-                         ("GUIX_PACKAGE_PATH" .
-                          "/home/vitaliy.kovalev/.dotfiles")))
-       (simple-service 'add-extra-hosts
-                       hosts-service-type
-                       (append
-                        vpn-servers
-                        personal-machines
-                        work-machines))
-       (service openssh-service-type
-                (openssh-configuration
-                  (port-number 13131)))
-       (service dhcpcd-service-type
-                (dhcpcd-configuration
-                  (interfaces '("enp1s0"))
-                  (option '("rapid_commit" "interface_mtu"))
-                  (no-option '("domain_name_servers" "domain_name" "domain_search" ))
-                  (no-hook '("resolv.conf"))
-                  (shepherd-provision '(dhcp-eltex))))
-       (service static-networking-service-type
-                (list (static-networking
-                        (addresses
-                         (list
-                          (network-address
-                            (device "enp2s0")
-                            (value "192.168.114.175/12"))))
-                        (routes
-                         (list
-                          (network-route
-                            (destination "192.168.104.0/21")
-                            (gateway "192.168.114.65"))))
-                        (name-servers '("192.168.107.61"))))))
-
-      (modify-services shit-trimmed-desktop-services
-        (delete network-manager-service-type)
-        (guix-service-type
-         config =>(guix-configuration
-                    (inherit config)
-                    (channels pinned-channels)
-                    (guix (guix-for-channels pinned-channels))
-                    (substitute-urls bordeaux-nonguix-substitute-urls)
-                    (authorized-keys default-authorized-keys-with-nonguix))))))
-
-
-    (privileged-programs
-     (append
+   (users
+    (cons*
+     (user-account
+      (name "vitaliy.kovalev")
+      (comment "Виталий Ковалёв")
+      (uid 1000)
+      (group "users")
+      (home-directory "/home/vitaliy.kovalev")
+      (supplementary-groups
+       '("wheel" "netdev" "audio" "video" "kvm" "libvirt" )))
+     %base-user-accounts))
+   ;;Globaly installed packages(e.g. for all users)
+   (packages
+    (append
+     (specifications->packages
       (cons*
-       (privileged-program
-         (program
-          (file-append (specification->package "iputils") "/bin/ping"));Neeeded for iptuils ping
-         (setuid? #t))
-       (privileged-program
-         (program
-          (file-append sngrep "/bin/sngrep"));Neeeded for iptuils ping
-         (capabilities "CAP_NET_RAW+eip"))
-       (assoc-ref virtualization-service-list "privileged-programs"))
-      %default-privileged-programs))
+       "eltex-certs"
+       (append
+        base-sys
+        base-gui
+        fs-tools
+        network-tools
+        virtualization-base
+        sys-fonts)))
+     %base-packages))
 
-    ;;Required for LVM disks
-    (mapped-devices
+   ;;Installed and enabled services(like ssh-server,docker, etc.)
+   (services
+    (append
+     (assoc-ref virtualization-service-list "services")
      (list
-      (mapped-device
-        (source "GawainLinux")
-        (targets
-         (list
-          "GawainLinux-GuixRoot"
-          "GawainLinux-GuixHome"
-          "GawainLinux-Swap"))
-        (type lvm-device-mapping))))
+      (service bluetooth-service-type)
+      (service iptables-service-type
+               (iptables-configuration
+                (ipv4-rules
+                 (local-file
+                  (string-append
+                   (getenv "DOTFILES_DIR")
+                   "/sys-files/gawain/iptables.rules")))
+                (ipv6-rules
+                 (local-file
+                  (string-append
+                   (getenv "DOTFILES_DIR")
+                   "/sys-files/gawain/iptables.rules")))))
+      (simple-service 'dotfiles-and-guix-env session-environment-service-type
+                      `(("DOTFILES_DIR" .
+                         "/home/vitaliy.kovalev/.dotfiles")
+                        ("GUIX_PACKAGE_PATH" .
+                         "/home/vitaliy.kovalev/.dotfiles")))
+      (simple-service 'add-extra-hosts
+                      hosts-service-type
+                      (append
+                       vpn-servers
+                       personal-machines
+                       work-machines))
+      (service openssh-service-type
+               (openssh-configuration
+                (port-number 13131)))
+      (service dhcpcd-service-type
+               (dhcpcd-configuration
+                (interfaces '("enp1s0"))
+                (option '("rapid_commit" "interface_mtu"))
+                (no-option '("domain_name_servers" "domain_name" "domain_search" ))
+                (no-hook '("resolv.conf"))
+                (shepherd-provision '(dhcp-eltex))))
+      (service static-networking-service-type
+               (list (static-networking
+                      (addresses
+                       (list
+                        (network-address
+                         (device "enp2s0")
+                         (value "192.168.114.175/12"))))
+                      (routes
+                       (list
+                        (network-route
+                         (destination "192.168.104.0/21")
+                         (gateway "192.168.114.65"))))
+                      (name-servers '("192.168.107.61"))))))
 
-    (file-systems
+     (modify-services shit-trimmed-desktop-services
+                      (delete network-manager-service-type)
+                      (guix-service-type
+                       config =>(guix-configuration
+                                 (inherit config)
+                                 (channels pinned-channels)
+                                 (guix (guix-for-channels pinned-channels))
+                                 (substitute-urls bordeaux-nonguix-substitute-urls)
+                                 (authorized-keys default-authorized-keys-with-nonguix))))))
+
+
+   (privileged-programs
+    (append
      (cons*
-      (file-system
-        (mount-point "/home")
-        (device (file-system-label "guix-home"))
-        (dependencies mapped-devices)
-        (type "ext4"))
-      (file-system
-        (mount-point "/")
-        (device (file-system-label "guix-root"))
-        (dependencies mapped-devices)
-        (type "ext4"))
-      (file-system
-        (mount-point "/boot/efi")
-        (device (file-system-label "EFI"))
-        (type "vfat"))
-      %base-file-systems))
+      (privileged-program
+       (program
+        (file-append (specification->package "iputils") "/bin/ping"));Neeeded for iptuils ping
+       (setuid? #t))
+      (privileged-program
+       (program
+        (file-append sngrep "/bin/sngrep"));Neeeded for iptuils ping
+       (capabilities "CAP_NET_RAW+eip"))
+      (assoc-ref virtualization-service-list "privileged-programs"))
+     %default-privileged-programs))
 
-    (swap-devices
-     (list
-      (swap-space
-        (target (file-system-label "swap"))
-        (dependencies mapped-devices))))))
+   ;;Required for LVM disks
+   (mapped-devices
+    (list
+     (mapped-device
+      (source "GawainLinux")
+      (targets
+       (list
+        "GawainLinux-GuixRoot"
+        "GawainLinux-GuixHome"
+        "GawainLinux-Swap"))
+      (type lvm-device-mapping))))
+
+   (file-systems
+    (cons*
+     (file-system
+      (mount-point "/home")
+      (device (file-system-label "guix-home"))
+      (dependencies mapped-devices)
+      (type "ext4"))
+     (file-system
+      (mount-point "/")
+      (device (file-system-label "guix-root"))
+      (dependencies mapped-devices)
+      (type "ext4"))
+     (file-system
+      (mount-point "/boot/efi")
+      (device (file-system-label "EFI"))
+      (type "vfat"))
+     %base-file-systems))
+
+   (swap-devices
+    (list
+     (swap-space
+      (target (file-system-label "swap"))
+      (dependencies mapped-devices))))))
 
 
 gawain-system
