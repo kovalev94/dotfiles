963c615f8343f0a52340cf7fdbfa5eb8e44a76df
Kovalev Vitaliy <kvp94best@gmail.com>
2025-12-02 17:48:38 +0700

    Change monitors and workspaces settings for exwm.

--- home-files/lulu/.config/exwm/monitors.el
+++ home-files/lulu/.config/exwm/monitors.el
@@ -1,6 +1,15 @@
 (provide 'monitors)
 
-(defvar exwm-randr-plist '(0 "eDP-1"))
+(defvar exwm-randr-plist '(0 "eDP-1"
+                           1 "eDP-1"
+                           2 "eDP-1"
+                           3 "eDP-1"
+                           4 "eDP-1"
+                           5 "eDP-1"
+                           6 "eDP-1"
+                           7 "eDP-1"
+                           8 "eDP-1"
+                           9 "eDP-1"))
 (defun exwm-change-screen ()
   (start-process
    "xrandr" nil "xrandr" "--output" "eDP-1" "--auto"))
--- home-files/vitaliy.kovalev/.config/exwm/monitors.el
+++ home-files/vitaliy.kovalev/.config/exwm/monitors.el
@@ -1,6 +1,15 @@
 (provide 'monitors)
 
-(defvar exwm-randr-plist '(0 "DP-1" 1 "HDMI-2"))
+(defvar exwm-randr-plist '(0 "DP-1"
+                           1 "DP-1"
+                           2 "DP-1"
+                           3 "DP-1"
+                           4 "DP-1"
+                           5 "HDMI-2"
+                           6 "HDMI-2"
+                           7 "HDMI-2"
+                           8 "HDMI-2"
+                           9 "HDMI-2"))
 (defun exwm-change-screen ()
   (start-process
    "xrandr" nil "xrandr" "--output" "DP-1" "--left-of" "HDMI-2" "--auto"))
