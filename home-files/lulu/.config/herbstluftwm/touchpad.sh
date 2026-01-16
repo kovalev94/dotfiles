fb9688edf91e6eca0a8fc0fa491533ed81044636
Kovalev Vitaliy <kvp94best@gmail.com>
2026-01-16 17:15:24 +0700

    Update herbstluftwm touchpad settings.

--- home-files/lulu/.config/herbstluftwm/touchpad.sh
+++ home-files/lulu/.config/herbstluftwm/touchpad.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-TouchpadDeviceID=9
-EnableTappingCode=$(xinput list-props "$TouchpadDeviceID"  \
-    |  grep  "libinput Tapping Enabled ([0-9][0-9][0-9])" \
-    | grep -oE "[0-9]{2,4}")
-xinput set-prop "$TouchpadDeviceID" $EnableTappingCode 1
+TOUCHPAD_DEV_NAME="GXTP7863:00 27C6:01E0 Touchpad"
+
+xinput set-prop "$TOUCHPAD_DEV_NAME" "libinput Tapping Enabled" 1
+xinput set-prop "$TOUCHPAD_DEV_NAME" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "$TOUCHPAD_DEV_NAME" "libinput Click Method Enabled" 0 1
