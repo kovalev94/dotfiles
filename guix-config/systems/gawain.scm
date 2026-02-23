662f871982ed9445114218fe766b242d2c115329
Kovalev Vitaliy <kvp94best@gmail.com>
2026-02-23 21:50:39 +0700

    Inherit mirage and gawain from new module base.

--- guix-config/systems/gawain.scm
+++ guix-config/systems/gawain.scm
@@ -30,32 +30,15 @@
   #:use-module (guix-config common)
   #:use-module (guix-config packages certs)
   #:use-module (guix-config packages telephony)
-  #:use-module (srfi srfi-1))
+  #:use-module (guix-config systems base)
+  #:use-module (srfi srfi-1)
+  #:export (gawain-system))
+;;Clean imports
 
-
-(define-public gawain-system
+(define gawain-system
   (operating-system
+    (inherit %my-desktop-base-system)
     (host-name "gawain")
-    (locale "ru_RU.utf8")
-    (timezone "Asia/Novosibirsk")
-
-    (bootloader
-      (bootloader-configuration
-        (bootloader grub-efi-bootloader)
-        (targets '("/boot/efi"))
-        (theme (grub-theme
-                (inherit (grub-theme))
-                (gfxmode '("1920x1080x32" "auto"))))
-        (keyboard-layout %my-kb-layout)))
-
-    (kernel linux)
-    (initrd microcode-initrd)
-    (firmware
-     (list
-      linux-firmware
-      sof-firmware))
-
-    (keyboard-layout %my-kb-layout)
 
     (users
      (cons*
@@ -66,125 +49,35 @@
         (group "users")
         (home-directory "/home/vitaliy.kovalev")
         (supplementary-groups
-         '("wheel" "netdev" "audio" "video" "kvm" "libvirt" )))
+         '("wheel" "netdev" "audio" "video" "kvm" "libvirt" "cgroup")))
       %base-user-accounts))
-    ;;Globaly installed packages(e.g. for all users)
-    (packages
-     (append
-      (specifications->packages
-       (cons*
-        "eltex-certs"
-        (append
-         base-sys
-         base-gui
-         fs-tools
-         network-tools
-         virtualization-base
-         sys-fonts)))
-      %base-packages))
 
-    ;;Installed and enabled services(like ssh-server,docker, etc.)
+    (packages
+     (cons*
+      (specification->package "eltex-certs")
+      (operating-system-packages %my-desktop-base-system)))
+
     (services
      (append
       (list
-       (service libvirt-service-type)
-       (service virtlog-service-type)
-       (simple-service
-        'spice-polkit polkit-service-type
-        (list spice-gtk))
-       (service nftables-service-type
-                (nftables-configuration
-                  (ruleset
-                   (local-file
-                    (string-append
-                     (getenv "DOTFILES_DIR")
-                     "/sys-files/gawain/nftables/rules")))))
        (simple-service 'my-env session-environment-service-type
                        `(("DOTFILES_DIR" .
                           "/home/vitaliy.kovalev/.dotfiles")
-                         ("TZ" . ,timezone)
+                         ("TZ" .
+                          ,(operating-system-timezone %my-desktop-base-system))
                          ("GUIX_PACKAGE_PATH" .
-                          "/home/vitaliy.kovalev/.dotfiles")))
-       (service rootless-podman-service-type
-                (rootless-podman-configuration
-                  (subgids
-                   (list (subid-range (name "vitaliy.kovalev"))))
-                  (subuids
-                   (list (subid-range (name "vitaliy.kovalev"))))))
-       (service openssh-service-type
-                (openssh-configuration
-                  (port-number 22)
-                  (extra-content "PermitTunnel yes"))))
-
-      (modify-services %desktop-services
-        (delete avahi-service-type)
-        (delete gdm-service-type)
-        (delete (service-kind gdm-file-system-service))
-        (guix-service-type
-         config =>(guix-configuration
-                    (inherit config)
-                    (channels %my-pinned-channels)
-                    (guix (guix-for-channels %my-pinned-channels))
-                    (substitute-urls %my-substitutes-urls)
-                    (authorized-keys %my-authorized-keys))))))
-
-    (privileged-programs
-     (cons*
-      (privileged-program
-        (program
-         (file-append
-          spice-gtk
-          "/libexec/spice-client-glib-usb-acl-helper"))
-        (setuid? #t))
-      (privileged-program
-        (program
-         (file-append sngrep "/bin/sngrep"))
-        (capabilities "CAP_NET_RAW+eip"))
-      (privileged-program
-        (program
-         (file-append (specification->package "iputils") "/bin/ping"))
-        (capabilities "cap_net_raw=ep"))
-      (remove (lambda (p)
-                (let ((path (object->string (privileged-program-program p))))
-                  (or (string-suffix? "/bin/ping" path)
-                      (string-suffix? "/bin/ping6" path))))
-              %default-privileged-programs)))
-
-    ;;Required for LVM disks
-    (mapped-devices
-     (list
-      (mapped-device
-        (source "GawainLinux")
-        (targets
-         (list
-          "GawainLinux-GuixRoot"
-          "GawainLinux-GuixHome"
-          "GawainLinux-Swap"))
-        (type lvm-device-mapping))))
-
-    (file-systems
-     (cons*
-      (file-system
-        (mount-point "/home")
-        (device (file-system-label "guix-home"))
-        (dependencies mapped-devices)
-        (type "ext4"))
-      (file-system
-        (mount-point "/")
-        (device (file-system-label "guix-root"))
-        (dependencies mapped-devices)
-        (type "ext4"))
-      (file-system
-        (mount-point "/boot/efi")
-        (device (file-system-label "EFI"))
-        (type "vfat"))
-      %base-file-systems))
-
-    (swap-devices
-     (list
-      (swap-space
-        (target (file-system-label "swap"))
-        (dependencies mapped-devices))))))
+                          "/home/vitaliy.kovalev/.dotfiles"))));; add dynamic behavior
+      (modify-services (operating-system-user-services %my-desktop-base-system)
+        (openssh-service-type
+         config => (openssh-configuration
+                     (extra-content "PermitTunnel yes")))
+        (nftables-service-type
+         config => (nftables-configuration
+                     (ruleset
+                      (local-file
+                       (string-append
+                        (getenv "DOTFILES_DIR");;
+                        "/sys-files/gawain/nftables/rules"))))))))));; add dynamic behavior
 
 
 gawain-system
--- guix-config/systems/mirage.scm
+++ guix-config/systems/mirage.scm
@@ -31,199 +31,79 @@
   #:use-module (guix-config channels)
   #:use-module (guix-config substitutes)
   #:use-module (guix-config common)
+  #:use-module (guix-config systems base)
   #:use-module (guix-config packages telephony)
   #:use-module (srfi srfi-1))
+;;Clean imports
 
 (define-public mirage-system
   (operating-system
-   (host-name "mirage")
-   (locale "ru_RU.utf8")
-   (timezone "Asia/Novosibirsk")
+    (inherit %my-desktop-base-system)
+    (host-name "mirage")
+    (locale "ru_RU.utf8")
+    (timezone "Asia/Novosibirsk")
+    ;;Temporary(I hope) fix for screen redraw lags
+    (kernel-arguments
+     (append
+      (list "i915.enable_psr=0")
+      %default-kernel-arguments))
 
-   (bootloader
-    (bootloader-configuration
-     (bootloader grub-efi-bootloader)
-     (targets '("/boot/efi"))
-     (theme (grub-theme
-             (inherit (grub-theme))
-             (gfxmode '("1920x1080x32" "auto"))))
-     (keyboard-layout %my-kb-layout)))
-
-   (kernel linux)
-   ;;Temporary(I hope) fix for screen redraw lags
-   (kernel-arguments
-    (append
-     (list "i915.enable_psr=0")
-     %default-kernel-arguments))
-
-   (initrd microcode-initrd)
-
-   (firmware
-    (list
-     linux-firmware
-     sof-firmware))
-
-
-   (keyboard-layout %my-kb-layout)
-
-
-   (users
-    (cons*
-     (user-account
-      (name "lulu")
-      (comment "Лелуш Ламперуж")
-      (uid 1000)
-      (group "users")
-      (home-directory "/home/lulu")
-      (supplementary-groups
-       '("wheel" "netdev" "audio" "video" "kvm" "libvirt" )))
-     %base-user-accounts))
-
-   ;;Globaly installed packages(e.g. for all users)
-   (packages
-    (append
-     (specifications->packages
-      (append
-       base-sys
-       base-gui
-       laptop
-       fs-tools
-       network-tools
-       virtualization-base
-       sys-fonts))
-     %base-packages))
-
-   ;;Installed and enabled services(like ssh-server,docker, etc.)
-   (services
-    (append
-     (list
-      (service libvirt-service-type)
-      (service virtlog-service-type)
-      (simple-service
-       'spice-polkit polkit-service-type
-       (list spice-gtk))
-      (service tlp-service-type)
-      (service thermald-service-type)
-      (service bluetooth-service-type)
-      (service nftables-service-type
-               (nftables-configuration
-                (ruleset
-                 (local-file
-                  (string-append
-                   (getenv "DOTFILES_DIR")
-                   "/sys-files/mirage/nftables/rules")))))
-      (simple-service 'my-env session-environment-service-type
-                      `(("DOTFILES_DIR" .
-                         "/home/lulu/.dotfiles")
-                        ("TZ" . ,timezone)
-                        ("GUIX_PACKAGE_PATH" .
-                         "/home/lulu/.dotfiles")))
-      (simple-service 'add-extra-hosts
-                      hosts-service-type
-                      (list
-                       (host "185.164.163.16" "vpnserv")
-                       (host "213.87.105.213" "damocles")
-                       (host "172.16.13.3" "lancelot")
-                       (host "109.174.98.182" "ipoint-marksa-white")
-                       (host "109.111.191.225" "ipoint-gogolya-white")
-                       (host "176.126.103.60" "ipoint-controller-white")))
-      (service rootless-podman-service-type
-               (rootless-podman-configuration
-                (subgids
-                 (list (subid-range (name "lulu"))))
-                (subuids
-                 (list (subid-range (name "lulu"))))))
-      (service openssh-service-type
-               (openssh-configuration
-                (port-number 13131))))
-
-     (modify-services %desktop-services
-       (delete avahi-service-type)
-       (delete gdm-service-type)
-       (delete (service-kind gdm-file-system-service))
-       ;;Sleep doesn't work, using freeze until fix.
-       (elogind-service-type
-        config =>(elogind-configuration
-                   (inherit config)
-                   (suspend-mode '(s2idle deep))))
-       (guix-service-type
-        config =>(guix-configuration
-                   (inherit config)
-                   (channels %my-pinned-channels)
-                   (guix (guix-for-channels %my-pinned-channels))
-                   (substitute-urls %my-substitutes-urls)
-                   (authorized-keys %my-authorized-keys)))
-       (network-manager-service-type config =>
-                                     (network-manager-configuration
-                                       (inherit config)
-                                       (vpn-plugins (list network-manager-openvpn))))
-       (console-font-service-type
-        _ => (map
-              (lambda
-                  (tty)
-                (cons
-                 tty
-                 (file-append
-                  font-terminus
-                  "/share/consolefonts/ter-k32n")))
-              '("tty1" "tty2" "tty3" "tty4" "tty5" "tty6"))))))
-
-    (privileged-programs
+    (users
      (cons*
-      (privileged-program
-        (program
-         (file-append
-          spice-gtk
-          "/libexec/spice-client-glib-usb-acl-helper"))
-        (setuid? #t))
-      (privileged-program
-        (program
-         (file-append sngrep "/bin/sngrep"))
-        (capabilities "CAP_NET_RAW+eip"))
-      (privileged-program
-        (program
-         (file-append (specification->package "iputils") "/bin/ping"))
-        (capabilities "cap_net_raw=ep"))
-      (remove (lambda (p)
-                (let ((path (object->string (privileged-program-program p))))
-                  (or (string-suffix? "/bin/ping" path)
-                      (string-suffix? "/bin/ping6" path))))
-              %default-privileged-programs)))
+      (user-account
+        (name "lulu")
+        (comment "Лелуш Ламперуж")
+        (uid 1000)
+        (group "users")
+        (home-directory "/home/lulu")
+        (supplementary-groups
+         '("wheel" "netdev" "audio" "video" "kvm" "libvirt" )))
+      %base-user-accounts))
 
-   (mapped-devices
-    (list
-     (mapped-device
-      (source "MirageLinux")
-      (targets
-       (list
-        "MirageLinux-GuixRoot"
-        "MirageLinux-GuixHome"
-        "MirageLinux-Swap"))
-      (type lvm-device-mapping))))
+    (services
+     (append
+      (list
+       (service tlp-service-type)
+       (service thermald-service-type)
+       (simple-service 'add-extra-hosts hosts-service-type
+                       (list
+                        (host "185.164.163.16" "vpnserv")
+                        (host "213.87.105.213" "damocles")
+                        (host "172.16.13.3" "lancelot")
+                        (host "109.174.98.182" "ipoint-marksa-white")
+                        (host "109.111.191.225" "ipoint-gogolya-white")
+                        (host "176.126.103.60" "ipoint-controller-white")))
+       (simple-service 'my-env session-environment-service-type
+                       `(("DOTFILES_DIR" .
+                          "/home/vitaliy.kovalev/.dotfiles")
+                         ("TZ" .
+                          ,(operating-system-timezone %my-desktop-base-system))
+                         ("GUIX_PACKAGE_PATH" .
+                          "/home/vitaliy.kovalev/.dotfiles"))));; add dynamic behavior
+      (modify-services (operating-system-user-services %my-desktop-base-system)
+        ;;Sleep doesn't work, using freeze until fix.
+        (elogind-service-type
+         config =>(elogind-configuration
+                    (inherit config)
+                    (suspend-mode '(s2idle deep))))
 
-   (file-systems
-    (cons*
-     (file-system
-      (mount-point "/home")
-      (device (file-system-label "guix-home"))
-      (dependencies mapped-devices)
-      (type "ext4"))
-     (file-system
-      (mount-point "/")
-      (device (file-system-label "guix-root"))
-      (dependencies mapped-devices)
-      (type "ext4"))
-     (file-system
-      (mount-point "/boot/efi")
-      (device (file-system-label "EFI"))
-      (type "vfat"))
-     %base-file-systems))
+        (nftables-service-type
+         config => (nftables-configuration
+                     (ruleset
+                      (local-file
+                       (string-append
+                        (getenv "DOTFILES_DIR");;
+                        "/sys-files/gawain/nftables/rules")))));; add dynamic behavior
+        (network-manager-service-type
+         config => (network-manager-configuration
+                     (inherit config)
+                     (vpn-plugins (list network-manager-openvpn))))
 
-   (swap-devices
-    (list
-     (swap-space
-      (target (file-system-label "swap"))
-      (dependencies mapped-devices))))))
+        (console-font-service-type
+         config => (map (lambda (pair)
+                          (cons (car pair)
+                                (file-append font-terminus "/share/consolefonts/ter-k32n")))
+                        config)))))))
 
 
 mirage-system
