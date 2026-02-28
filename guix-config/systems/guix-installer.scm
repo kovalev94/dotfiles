8f02a01c9e3c29a92291c7235e0205a691fa2a86
Kovalev Vitaliy <kvp94best@gmail.com>
2026-03-01 01:21:44 +0700

    Inherit guix-installer from nonguix installer.

--- guix-config/systems/guix-installer.scm
+++ guix-config/systems/guix-installer.scm
@@ -1,53 +1,25 @@
 (define-module (guix-config systems guix-installer)
-  #:use-module (gnu services)
   #:use-module (gnu system)
-  #:use-module (gnu system install)
-  #:use-module (gnu bootloader)
-  #:use-module (gnu bootloader grub)
-  #:use-module (gnu services base)
-  #:use-module (gnu packages package-management)
-  #:use-module (nongnu packages linux)
+  #:use-module (nongnu system install)
   #:use-module (guix-config packages)
-  #:use-module (guix-config channels)
-  #:use-module (guix-config substitutes)
-  #:use-module (guix-config keyboard))
+  #:use-module (guix-config keyboard)
+  #:export (guix-installer-system))
 
 
 (define-public guix-installer-system
   (operating-system
-   (inherit installation-os)
+    (inherit installation-os-nonfree)
 
-   (host-name "guix-installer")
-   (locale "ru_RU.utf8")
-   (timezone "Asia/Novosibirsk")
+    (host-name "guix-installer")
+    (locale "ru_RU.utf8")
+    (timezone "Asia/Novosibirsk")
 
-   (bootloader
-    (bootloader-configuration
-     (bootloader grub-bootloader)
-     (targets '("/dev/sda"))
-     (keyboard-layout %my-kb-layout)))
+    (keyboard-layout %my-kb-layout)
 
-   (firmware
-    (list
-     linux-firmware
-     sof-firmware))
-
-   (keyboard-layout %my-kb-layout)
-
-   (packages
-    (append
-     %my-base-packages
-     (operating-system-packages installation-os)))
-
-   (services
-     (modify-services (operating-system-user-services installation-os)
-       (guix-service-type
-        config =>(guix-configuration
-                  (inherit config)
-                  (channels %my-pinned-channels)
-                  (guix (guix-for-channels %my-pinned-channels))
-                  (substitute-urls %my-substitutes-urls)
-                  (authorized-keys %my-authorized-keys)))))))
+    (packages
+     (append
+      %my-base-packages
+      (operating-system-packages installation-os-nonfree)))))
 
 
 guix-installer-system
