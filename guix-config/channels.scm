374134923485fd3fb47c95842a35efd758284e55
Kovalev Vitaliy <kvp94best@gmail.com>
2026-03-16 23:08:15 +0700

    Add rosenthal channel & guix-moe substitute server

--- guix-config/channels.scm
+++ guix-config/channels.scm
@@ -1,6 +1,7 @@
 (define-module (guix-config channels)
   #:use-module (guix channels)
   #:export (%nonguix-channel
+            rosenthal-channel
             %my-channels
             %my-pinned-channels)
   #:re-export (%default-guix-channel))
@@ -17,10 +18,22 @@
       (openpgp-fingerprint
        "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
 
+(define rosenthal-channel
+  (channel
+    (name 'rosenthal)
+    (url "https://codeberg.org/hako/rosenthal.git")
+    (branch "trunk")
+    (introduction
+     (make-channel-introduction
+      "7677db76330121a901604dfbad19077893865f35"
+      (openpgp-fingerprint
+       "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7")))))
+
 (define %my-channels
   (list
    %default-guix-channel
-   %nonguix-channel))
+   %nonguix-channel
+   rosenthal-channel))
 
 (define %my-pinned-channels
   (list (channel
